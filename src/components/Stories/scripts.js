47416926415c90083b61adbe0f4edf519d1b3dde
Oleksandr Borysenko <kontynium@upstars.com>
2025-02-04 22:01:30 +0100

    remowe unsed segments

--- src/components/Stories/scripts.js
+++ src/components/Stories/scripts.js
@@ -1,5 +1,3 @@
-
-
 import { ref, computed, onMounted } from "vue";
 import StoriesTopBar from "@components/Stories/UI/storiesTopBar.vue";
 import mobileControlArea from "@components/Stories/UI/mobileControlArea.vue";
@@ -17,11 +15,6 @@ import bottom_gradient1 from "@components/Stories/img/bottom_gradient1.svg";
 import icon_replay from "@components/Stories/img/icons/icon_replay.svg";
 import video_bg from "@components/Stories/img/video.mp4";
 import video_bg_webm from "@components/Stories/img/video.webm";
-
-
-
-
-
 
 export default {
     name: "Bonuses",
@@ -46,21 +39,12 @@ export default {
         const segment2_time = ref(0);
         const segment3_time = ref(0);
         const segment4_time = ref(0);
-        const segment5_time = ref(0);
-        const segment6_time = ref(0);
-        const segment7_time = ref(0);
-        const segment8_time = ref(0);
         const segment1_duration = ref(0);
         const segment2_duration = ref(0);
         const segment3_duration = ref(0);
         const segment4_duration = ref(0);
-        const segment5_duration = ref(0);
-        const segment6_duration = ref(0);
-        const segment7_duration = ref(0);
-        const segment8_duration = ref(0);
         const video = ref(null);
         const end_link = ref('https://winspirit3.com/registration');
-
 
         const segment1 = gsap.timeline({
             defaults: { duration: defaultDuration, ease: "power1.inOut" },
@@ -86,30 +70,6 @@ export default {
                 segment4_time.value = segment4.time();
             }
         });
-        const segment5 = gsap.timeline({
-            defaults: { duration: defaultDuration, ease: "power1.inOut" },
-            onUpdate: () => {
-                segment5_time.value = segment5.time();
-            }
-        });
-        const segment6 = gsap.timeline({
-            defaults: { duration: defaultDuration, ease: "power1.inOut" },
-            onUpdate: () => {
-                segment6_time.value = segment6.time();
-            }
-        });
-        const segment7 = gsap.timeline({
-            defaults: { duration: defaultDuration, ease: "power1.inOut" },
-            onUpdate: () => {
-                segment7_time.value = segment7.time();
-            }
-        });
-        const segment8 = gsap.timeline({
-            defaults: { duration: defaultDuration, ease: "power1.inOut" },
-            onUpdate: () => {
-                segment8_time.value = segment8.time();
-            }
-        });
         const texts = ref('en');
         const currency = ref('EUR');
         const name = ref('');
@@ -125,7 +85,7 @@ export default {
         const isPlaying = ref(true);
         const isPaused = ref(false);
         const thumbs_part = ref(0);
-        const numberOfSegments = ref(6);
+        const numberOfSegments = ref(4);
         const isPlayingHasBeenSet = ref(false);
         const animationPauseStyle = computed(() => ({
             "animation-play-state": isPaused.value ? "paused" : "running",
@@ -140,7 +100,6 @@ export default {
         const updateTime = () => {
             currentTime.value = tl.time;
             duration.value = tl.duration;
-
         };
         const progress = computed(() => {
             let result = 0;
@@ -170,34 +129,8 @@ export default {
                 totalSegments++;
             }
 
-            let segment5Progress = 0;
-            if (segment5_duration.value > 0) {
-                segment5Progress = segment5_time.value / segment5_duration.value;
-                totalSegments++;
-            }
-
-            let segment6Progress = 0;
-            if (segment6_duration.value > 0) {
-                segment6Progress = segment6_time.value / segment6_duration.value;
-                totalSegments++;
-            }
-
-            let segment7Progress = 0;
-            if (segment7_duration.value > 0) {
-                segment7Progress = segment7_time.value / segment7_duration.value;
-                totalSegments++;
-            }
-
-            let segment8Progress = 0;
-            if (segment8_duration.value > 0) {
-                segment8Progress = segment8_time.value / segment8_duration.value;
-                totalSegments++;
-            }
-
             if (totalSegments > 0) {
-                const computedValue = (segment1Progress + segment2Progress +
-                        segment3Progress  + segment4Progress + segment5Progress + segment6Progress +
-                        segment7Progress + segment8Progress)
+                const computedValue = (segment1Progress + segment2Progress + segment3Progress + segment4Progress)
                     / totalSegments * 100;
                 numberOfSegments.value = totalSegments;
                 if (!isNaN(computedValue)) {
@@ -205,7 +138,7 @@ export default {
                 }
             }
 
-            if (segment8_time.value > 0 && !reach_end.value) {
+            if (segment4_time.value > 0 && !reach_end.value) {
                 gtag('event', 'reach_end', {
                     'event_category': 'user_session',
                     'event_label': 'reach_end'
@@ -216,70 +149,48 @@ export default {
             return result;
         });
 
-
-
-
         const segmentStartTimes = computed(() => [
-            0, // The first segment starts at 0
+            0,
             segment1_duration.value,
             segment1_duration.value + segment2_duration.value,
             segment1_duration.value + segment2_duration.value + segment3_duration.value,
             segment1_duration.value + segment2_duration.value + segment3_duration.value + segment4_duration.value,
-            segment1_duration.value + segment2_duration.value + segment3_duration.value + segment4_duration.value + segment5_duration.value,
-            segment1_duration.value + segment2_duration.value + segment3_duration.value + segment4_duration.value + segment5_duration.value + segment6_duration.value,
-            segment1_duration.value + segment2_duration.value + segment3_duration.value + segment4_duration.value + segment5_duration.value + segment6_duration.value + segment7_duration.value,
         ]);
 
         const jumpToSegment = (direction) => {
-            // Calculate the current segment based on the start times
             let currentSegment = segmentStartTimes.value.findIndex((startTime, i) => {
                 return currentTime.value >= startTime && currentTime.value < segmentStartTimes.value[i + 1];
             });
 
             if (direction === "backward") {
-
-                // If it's the first segment, do not jump to the last one
                 if (currentSegment === -1) {
                     let newTime = segmentStartTimes.value[numberOfSegments.value - 2];
                     tl.time(newTime + defaultDuration);
                     video.value.currentTime = newTime + defaultDuration;
                 } else {
-
-                let newTime = segmentStartTimes.value[currentSegment - 1];
-                if (newTime < 0) {
-                    newTime = 0;
+                    let newTime = segmentStartTimes.value[currentSegment - 1];
+                    if (newTime < 0) {
+                        newTime = 0;
+                    }
+                    tl.time(newTime + defaultDuration);
+                    video.value.currentTime = newTime + defaultDuration;
                 }
-                tl.time(newTime + defaultDuration);
-                video.value.currentTime = newTime + defaultDuration;
-                }
-
-
                 gtag('event', 'click_backward', {
                     'event_category': 'button',
                     'event_label': 'backward'
                 });
             } else if (direction === "forward") {
-
-                // If it's the last segment or beyond, do not jump to the first one
                 if (currentSegment === -1) {
                     return;
                 }
                 if (currentSegment === numberOfSegments.value - 1) {
-                    
                     return;
                 }
-
                 let newTime = segmentStartTimes.value[currentSegment + 1];
                 tl.time(newTime);
                 video.value.currentTime = newTime + defaultDuration;
-                // gtag('event', 'click_forward', {
-                //     'event_category': 'button',
-                //     'event_label': 'forward'
-                // });
             }
         };
-
-
 
         const closeStory = () => {
             gtag('event', 'close_story', {
@@ -295,9 +206,9 @@ export default {
                 'event_category': 'button',
                 'event_label': 'watch_again'
             });
-                setTimeout(() => {
-            window.location.reload();
-                }, 150);
+            setTimeout(() => {
+                window.location.reload();
+            }, 150);
         };
 
         const regButton = () => {
@@ -308,7 +219,6 @@ export default {
             setTimeout(() => {
                 window.location.href = end_link.value;
             }, 150);
-            
         };
         let pauseTimer = null;
 
@@ -317,12 +227,9 @@ export default {
             isPaused.value = true;
             tl.pause();
             video.value.pause();
-            // Clear any existing timer
             if (pauseTimer) {
                 clearTimeout(pauseTimer);
             }
-
-            // Start a new timer
             pauseTimer = setTimeout(() => {
                 if (!isPlaying.value) {
                     if (currentTime.value > 0.6) {
@@ -382,7 +289,6 @@ export default {
 
         const release = (direction) => {
             clearTimeout(pressTimer.value);
-
             if (longPress.value) {
                 playerPlay();
             } else {
@@ -401,7 +307,6 @@ export default {
             }
         };
 
-
         const handleEventEnd = (direction, event) => {
             if (event.type === "touchend") {
                 event.preventDefault();
@@ -416,8 +321,6 @@ export default {
                 let vh = Math.round(window.innerHeight / 100);
                 document.documentElement.style.setProperty("--vh", `${vh}px`);
             });
-
-
 
             const fullURL = window.location.href;
             const queryStartIndex = fullURL.indexOf('?');
@@ -452,16 +355,13 @@ export default {
                 }
 
             } else {
-
                 const defaultLanguage = navigator.language.split('-')[0];
                 if (availableLanguages.languages.includes(defaultLanguage)) {
                     texts.value = defaultLanguage;
                 }
-
             }
 
             const locale = texts.value;
-
             if (availableLanguages.languages.includes(locale)) {
                 texts.value = languageMap[locale];
             } else {
@@ -474,14 +374,11 @@ export default {
                 }
             }
 
-
-
-            // SEGMENT 1
-                segment1.add(() => {
-                    video.value.autoplay = true;
-                    video.value.load();
-                    video.value.play();
-                }, 0);
+            segment1.add(() => {
+                video.value.autoplay = true;
+                video.value.load();
+                video.value.play();
+            }, 0);
             segment1.fromTo(["#text_1_1", "#text_1_2"],
                 {
                     opacity: 0,
@@ -518,47 +415,36 @@ export default {
                     duration: 0.5,
                     stagger: 0.3
                 });
-
-                segment1.to(["#text_1_3", "#text_1_4"],
+            segment1.to(["#text_1_3", "#text_1_4"],
                 {
                     opacity: 0,
                     duration: 0.5,
                     delay: 1.5
                 });
-                
             segment1_duration.value = segment1.duration();
 
-
-
-            // SEGMENT 2
-
-
             segment2.fromTo(["#text_2_1", "#text_2_2"],
-                    {
-                        opacity: 0,
-                        marginTop: "8%",
-                        ease: "power1.inOut",
-                        duration: 0.5,
-                        stagger: 0.3
-                    },
-                    {
-                        opacity: 1,
-                        marginTop: "0",
-                        ease: "power1.inOut",
-                        duration: 0.5,
-                        stagger: 0.3
-                    });
-
-            segment2.to(["#text_2_1", "#text_2_1"],
-                        {
-                            opacity: 0,
-                            duration: 0.5,
-                            delay: 5
-                        });
-
+                {
+                    opacity: 0,
+                    marginTop: "8%",
+                    ease: "power1.inOut",
+                    duration: 0.5,
+                    stagger: 0.3
+                },
+                {
+                    opacity: 1,
+                    marginTop: "0",
+                    ease: "power1.inOut",
+                    duration: 0.5,
+                    stagger: 0.3
+                });
+            segment2.to(["#text_2_1", "#text_2_2"],
+                {
+                    opacity: 0,
+                    duration: 0.5,
+                    delay: 5
+                });
             segment2_duration.value = segment2.duration();
-
-            // SEGMENT 3
 
             segment3.fromTo(["#text_3_1", "#text_3_2"],
                 {
@@ -575,19 +461,13 @@ export default {
                     duration: 0.5,
                     stagger: 0.3
                 });
-
             segment3.to(["#text_3_1", "#text_3_2"],
                 {
                     opacity: 0,
                     duration: 0.5,
                     delay: 7
                 });
-
             segment3_duration.value = segment3.duration();
-
-            // SEGMENT 4
-
-
 
             segment4.fromTo(["#text_4_1", "#text_4_2"],
                 {
@@ -604,17 +484,11 @@ export default {
                     duration: 0.5,
                     stagger: 0.3
                 });
-
             segment4.to(["#text_4_1", "#text_4_2"],
                 {
                     delay: 15
                 });
-
-
             segment4_duration.value = segment4.duration();
-
-          
-
 
             tl.add(segment1);
             tl.add(segment2);
@@ -622,11 +496,7 @@ export default {
             tl.add(segment4);
             tl.play();
 
-
-
-
             duration.value = tl.duration();
-
         });
 
         return {
@@ -663,16 +533,4 @@ export default {
             regButton
         };
     },
-    methods: {
-        reloadPage() {
-            gtag('event', 'click_replay', {
-                'event_category': 'button',
-                'event_label': 'replay'
-            });
-            setTimeout(() => {
-                window.location.reload();
-            }, 150);
-        }
-    }
-
 };
